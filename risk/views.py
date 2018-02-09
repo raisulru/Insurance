ea83780f9fa4f29ba9fd68e939f732cde0aa6008
raisulru <raisulru10@gmail.com>
2018-02-09 17:57:44 +0600

    Added all serializers and write list and details views

--- risk/views.py
+++ risk/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import RiskSerializer
+from .serializers import RiskListSerializer, RiskPostSerializer
 from .models import Risk
 
 
 # Create your views here.
 class RiskList(generics.ListCreateAPIView):
     queryset = Risk.objects.all()
-    serializer_class = RiskSerializer
+    serializer_class = RiskListSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return RiskPostSerializer
+        return RiskListSerializer
+
+
+class RiskDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Risk.objects.all()
+    serializer_class = RiskListSerializer
